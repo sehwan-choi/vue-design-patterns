4a55e2f720846f3736d22b7955625de7a2e72e27
sehwan <nu940907@gmail.com>
2023-09-30 13:47:58 +0900

    common Approach - design patterns

---
+++ common-pattern/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <app-header :title="headerTitle"></app-header>
+    <app-content :items="getItems" v-on:increase="add"></app-content>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppContent from './components/AppContent.vue'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      headerTitle: 'Common Approach',
+      datas: [{
+        num : 10,
+        id:1
+      }]
+    }
+  },
+  components: {
+    AppHeader,
+    AppContent
+  },
+  computed: {
+    getItems() {
+      return this.datas;
+    }
+  },
+  methods: {
+    add() {
+      var beforeNum = this.datas[this.datas.length - 1].num;
+      var beforeId = this.datas[this.datas.length - 1].id;
+      var data = { num : beforeNum += 10, 
+                   id : beforeId += 1 };
+      this.datas.push(data);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ common-pattern/src/components/AppContent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ul v-for="(item) in items" :key="item.id">
+        <li> {{ item.id }} - {{ item.num }}</li>
+    </ul>
+    <button @click="$emit('increase')">renew items</button>
+    <button @click="num++">add num</button> {{  num }}
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            num :0
+        }
+    },
+    props: {
+        items: {
+            type: Array,
+            required: true,
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
