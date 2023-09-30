f7ed5feff0d299e9cbc20d254294fa1e4bc5c13c
sehwan <nu940907@gmail.com>
2023-09-30 14:32:04 +0900

    slot pattern

---
+++ slot-pattern/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <ul>
+      <item-view>
+        아이템 1
+        <div slot="body">
+          아이템 1 바디
+          <button>click me</button>
+        </div>
+      </item-view>
+      <item-view>
+        아이템 2
+          <div slot="header">
+            헤더
+          </div>
+          <div slot="footer">
+            푸터
+          </div>
+      </item-view>
+      <item-view>아이템 3</item-view>
+      <item-view>아이템 4</item-view>
+      <item-view>아이템 5</item-view>
+    </ul>
+  </div>
+</template>
+
+<script>
+import ItemView from './components/ItemView.vue'
+
+export default {
+  components: { ItemView },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ slot-pattern/src/components/ItemView.vue
@@ -0,0 +1,21 @@
+<template>
+  <li>
+    <slot>
+
+    </slot>
+
+    <slot name="header"></slot>
+    <slot name="body"></slot>
+    <slot name="footer"></slot>
+  </li>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
