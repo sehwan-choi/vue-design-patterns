ef7dd71a34de9cf8d954b2c8ac68dd88e9c7303a
sehwan <nu940907@gmail.com>
2023-09-30 16:01:35 +0900

    renderless component

---
+++ renderless-component/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <fetch-data url="https://jsonplaceholder.typicode.com/users/1">
+      <div slot-scope="{ response, loading }">
+        <div v-if="!loading">
+          {{ response }}
+        </div>
+        <div v-if="loading">
+          Loading...
+        </div>
+      </div>
+    </fetch-data>
+  </div>
+</template>
+
+<script>
+import FetchData from './components/FetchData.vue'
+export default {
+  components: { FetchData },
+
+}
+</script>
+
+<style>
+
+</style>
