2fd44f9b813280c4c81f4c072d586c862ce821e9
sehwan <nu940907@gmail.com>
2023-09-30 15:04:35 +0900

    Controlled Component Pattern

---
+++ controlled-component/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <check-box v-model="checked"></check-box>
+  </div>
+</template>
+
+<script>
+import CheckBox from './components/CheckBox.vue'
+
+export default {
+  components: {
+    CheckBox
+  },
+  data() {
+    return {
+      checked: false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ controlled-component/src/components/CheckBox.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <input type="checkbox" :value="value" @click="toggleCheckBox">
+        {{ value }}
+  </div>
+  
+</template>
+
+<script>
+export default {
+    props: {
+        value: Boolean
+    },
+    methods: {
+        toggleCheckBox() {
+            this.$emit('input', !this.value);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
